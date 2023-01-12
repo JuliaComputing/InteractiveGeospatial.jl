e966c65ed0661818a1f6c060877cf988f36926c3
Josh Day <emailjoshday@gmail.com>
2023-01-12 14:49:14 -0500

    simplify template generation

--- src/InteractiveGeo.jl
+++ src/InteractiveGeo.jl
@@ -7,11 +7,16 @@ function __init__()
     global DIR = joinpath(@get_scratch!("InteractiveGeo-user"))
 end
 
+function scratchfile(src)
+    file = joinpath(DIR, basename(src))
+    rm(file, force=true)
+    cp(src, file)
+end
+
 
 "Load raster data, draw a polygon layer, and save it as GeoJSON."
 function draw_polygon()
-    file = cp(joinpath(@__DIR__, "..", "notebooks", "draw_polygon.jl"), joinpath(DIR, "draw_polygon.jl"))
-    Pluto.run(notebook=file)
+    Pluto.run(notebook=scratchfile(joinpath(@__DIR__, "..", "notebooks", "draw_polygon.jl")))
 end
 
 
