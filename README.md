c7b2932cc9d6b7222e3c3d3640d02a8f0e3d7c26
Josh Day <emailjoshday@gmail.com>
2023-01-12 14:44:11 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # InteractiveGeo
+
+**InteractiveGeo** is a Julia package that provides interactive tools (Pluto notebooks, etc.) to
+aid in working with geospatial data.
--- src/InteractiveGeo.jl
+++ src/InteractiveGeo.jl
@@ -1,5 +1,18 @@
 module InteractiveGeo
 
-# Write your package code here.
+using Scratch
+using Pluto
 
+function __init__()
+    global DIR = joinpath(@get_scratch!("InteractiveGeo-user"))
 end
+
+
+"Load raster data, draw a polygon layer, and save it as GeoJSON."
+function draw_polygon()
+    file = cp(joinpath(@__DIR__, "..", "notebooks", "draw_polygon.jl"), joinpath(DIR, "draw_polygon.jl"))
+    Pluto.run(notebook=file)
+end
+
+
+end #module
