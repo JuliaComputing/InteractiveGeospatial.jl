5b170e55973bf36cfa316deb5717c1b169ee6130
Josh Day <emailjoshday@gmail.com>
2024-09-10 02:56:33 -0400

    workaround for segfault, fix geojson serialization

--- src/InteractiveGeospatial.jl
+++ src/InteractiveGeospatial.jl
@@ -1,6 +1,6 @@
 module InteractiveGeospatial
 
-using GLMakie, Rasters, Observables, Markdown, JSON3
+using GLMakie, Rasters, Observables, Markdown, JSON3, Statistics
 using OrderedCollections: OrderedDict
 
 export draw_features, @md_str, geojson
@@ -20,7 +20,7 @@ function prepare_geojson_write(feat::PolygonFeature)
     OrderedDict(
         "type" => "Feature",
         "geometry" => OrderedDict(
-            "type" => "Polygon",
+            "type" => "LineString",
             "coordinates" => feat.coordinates
         ),
         "properties" => OrderedDict(
@@ -44,15 +44,16 @@ function draw_features(r)
     r = Rasters._maybe_resample(r)  # Remove rotation if rotated: AffineMap => LinRange Axes
     x_extrema, y_extrema = extrema.(r.dims)
     top_left = x_extrema[1], y_extrema[2]
+    top_right = x_extrema[2], y_extrema[2]
     ui_width = 200
-    size = (1300, 1000)
 
     # Initialize Figure
-    fig = Figure(; size)
+    fig = Figure(; size = (1300, 1000))
+    display(fig)
     ax = Axis(fig[1, 2])
     rowsize!(fig.layout, 1, Relative(1))
 
-    # Observables
+    # clicks and related observables
     mouse_coords = Observable((x=0f0, y=0f0))  # where mouse is hovering in terms of raster coordinates
     click_coords = Observable(Point2f[])
     dotted_line = @lift isempty($click_coords) ? Point2f[] : [first($click_coords), last($click_coords)]
@@ -66,18 +67,34 @@ function draw_features(r)
         end
     end
 
-    # Create UI Observables
-    draw_color = Menu(fig; options=[:white, :black, :red, :blue, :green, :orange, :gray])
-    remap_fun = Menu(fig; options=[cbrt, identity, sqrt, cbrt, pseudolog10, symlog10])
-    colormap = Menu(fig; options=[:viridis, :grays, :ground_cover, :inferno, :plasma, :magma, :tokyo, :devon, :hawaii, :buda, :RdBu, :BrBg])
+    #### UI Observables ####
+    # UI Helper Functions
+    menu(options...) = Menu(fig; options=collect(options), width=ui_width)
+    button(label) = Button(fig; label, width=ui_width)
+    # `padding = (left, right, bottom, top)`
+    label(text) = Label(fig, text, fontsize=18, halign = :left, padding=(0,0,-5,5), color=:black)
+    sublabel(text) = Label(fig, text, fontsize=10, halign=:left, padding = (0,0,-15,0), color=:gray)
 
-    polygon2view = Menu(fig; options=["none"])
+    # `aggregate` Inputs
+    init_scale = round(Int, maximum(size(r)) / 1000)
+    scale = Textbox(fig; stored_string=string(init_scale), validator=Int, width=ui_width)
+    aggregate_fun = menu(sum, mean, minimum, maximum)
+
+    # `aggregate` Output (Raster plotted with heatmap)
+    A = Observable{AbstractMatrix}(aggregate(sum, r, init_scale))
+    onany(aggregate_fun.selection, scale.stored_string) do fun, scale
+        A[] = aggregate(fun, r, parse(Int, scale))
+    end
+
+    draw_color = menu(:white, :black, :red, :blue, :green, :orange, :gray)
+    remap_fun = menu(cbrt, identity, sqrt, cbrt, pseudolog10, symlog10)
+    colormap = menu(:viridis, :grays, :ground_cover, :inferno, :plasma, :magma, :tokyo, :devon, :hawaii, :buda, :RdBu, :BrBg)
+
+    polygon2view = menu("none")
     shape = @lift $(polygon2view.selection) == "none" ? Point2f[] : features[][$(polygon2view.selection)].coordinates
 
-    max_res = Textbox(fig; stored_string="1000", validator=Int, width=ui_width)
-    A = @lift Rasters._subsample(r, parse(Int, $(max_res.stored_string)))
 
-    clear_btn = Button(fig; label="Clear", width=ui_width)
+    clear_btn = button("Clear")
     on(clear_btn.clicks) do _
         click_coords[] = []
         messages.text[] = ""
@@ -85,7 +102,7 @@ function draw_features(r)
 
     polygon_label = Textbox(fig; placeholder="Polygon Label", width=ui_width, validator= x -> x ∉ keys(features[]))
 
-    save_btn = Button(fig; label="Save", width=ui_width)
+    save_btn = button("Save")
     on(save_btn.clicks) do _
         if length(click_coords[]) > 2
             feat = PolygonFeature(polygon_label.stored_string[], [click_coords[]..., click_coords[][1]], md"")
@@ -95,7 +112,7 @@ function draw_features(r)
             push!(polygon2view.options[], feat.label)
             notify(polygon2view.options)
             messages.text[] = "Polygon Saved: \"$(feat.label)\""
-            @info "Polygon Saved"
+            @info "Polygon Saved: \"$(feat.label)\""
         else
             messages.color = :red
             messages.text[] = "Polygon must have at least 3 vertices"
@@ -104,27 +121,30 @@ function draw_features(r)
 
     messages = Label(fig, ""; color=:green)
 
-    # padding = (left, right, bottom, top)
-    label(text) = Label(fig, text, fontsize=18, halign = :left, padding=(0,0,0,20))
-    sublabel(text) = Label(fig, text, fontsize=14, halign=:left, padding = (0,0,0,10))
-    spacer = Label(fig, "")
+
 
     # UI Layout
-    fig[1, 1] = vgrid!(valign=:top, width=ui_width,
-        label("Maximum Resolution"),
-            max_res,
-        label("Remap Function"),
+    fig[1, 1] = vgrid!(valign=:top, halign=:left, width=1.2ui_width,
+        label("Display Settings ($(size(r, 1)) × $(size(r, 2)))"),
+        sublabel("Aggregate Function"),
+        aggregate_fun,
+        sublabel("Aggregate Scale"),
+        scale,
+        sublabel("Remap Function"),
             remap_fun,
-        label("Colormap"),
+        sublabel("Colormap"),
             colormap,
         label("Draw Polygon"),
+            sublabel("Color"),
             draw_color,
+            sublabel("Clear Current Drawing"),
             clear_btn,
-            sublabel("Enter Unique Label and Press Enter:"),
+            sublabel("Label (Unique Identifier)"),
             polygon_label,
+            sublabel("Save to Return Value"),
             save_btn,
             messages,
-        label("View Saved Polygon"),
+        label("View Drawing"),
             polygon2view,
     )
 
@@ -133,20 +153,22 @@ function draw_features(r)
     Colorbar(fig[1, 3], h)
 
     # Mouse position in top left corn
-    text!(ax, top_left..., text=@lift(string($mouse_coords)), color=draw_color.selection, align=(:left, :top), fontsize=12)
+    text!(ax, top_left..., text=@lift(string(' ', map(Float64, ($mouse_coords)))), color=draw_color.selection, align=(:left, :top), fontsize=12)
+
+    # Resolution
+    text!(ax, top_right..., text=@lift(string("Resolution: ", size($A, 1), " × ", size($A, 2), ' ')), color=draw_color.selection, align=(:right, :top), fontsize=12)
 
     # Polygon Drawing
-    # TODO FIXME This segfaults when uncommented...well shit.
-    # scatterlines!(ax, click_coords, color=draw_color.selection, linewidth=2, markersize=20)
-    # lines!(ax, dotted_line, color=draw_color.selection, linewidth=2, linestyle=:dash)
+    scatterlines!(ax, click_coords, color=draw_color.selection, markersize=10)
+    lines!(ax, dotted_line, color=draw_color.selection, linestyle=:dash)
 
-    # # Polygon Viewing
-    # poly!(ax, shape, color=draw_color.selection, linestyle=(:dot, :dense), alpha=.3)
+    # Polygon Viewing
+    poly!(ax, shape, color=draw_color.selection, linestyle=(:dot, :dense), alpha=.3)
 
     resize_to_layout!(fig)
-    display(fig)
 
-    return features
+
+    return (; features, click_coords)
 end
 
 end #module
